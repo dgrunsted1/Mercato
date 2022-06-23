cc3d3fee532686486d883c8d9ad4f6f26a6281b2
dgrunsted1 <dgrunsted1@gmail.com>
2022-06-23 17:57:06 -0500

    visual start kinda

--- src/routes/travel.svelte
+++ src/routes/travel.svelte
@@ -12,10 +12,145 @@
     }
   </script>
 <script>
+    let trip_config = {
+        default: {value: true, type:"none"}, 
+        start_date: {value: "", type:"date"}, 
+        return_date: {value: "", type:"date"}, 
+        start_location: {value: "", type:"text"}
+    };
 
-    let entry_types = ['activity', 'travel', 'stay'];
-    let entries = [{}];
+
+    // for testing
+    trip_config = {
+        default: {value: false, type:"none"}, 
+        start_date: {value: "2022-06-20", type:"date"}, 
+        return_date: {value: "2022-07-05", type:"date"}, 
+        start_location: {value: "Milwaukee, WI, USA", type:"text"}
+    }
+
+
+    let activities = {};
+    let travel = {};
+    let entry_types = [ 'travel', 'activity', 'stay'];
+    let entries = {
+        travel: [
+            {
+                title: "flight to italy",
+                departure_date: "2022-06-20T04:48",
+                arrival_date: "2022-06-20T16:48",
+                departure_location: "chicago",
+                arrival_location: "rome"
+
+            },
+            {
+                title: "flight home from italy",
+                departure_date: "2022-07-05T04:48",
+                arrival_date: "2022-07-05T16:48",
+                departure_location: "Rome",
+                arrival_location: "Chicago"
+            },
+            {
+                title: "train to florence",
+                departure_date: "2022-06-23T08:48",
+                arrival_date: "2022-06-23T11:48",
+                departure_location: "Rome",
+                arrival_location: "florence"
+            },
+            {
+                title: "rental car to rietine",
+                departure_date: "2022-06-23T13:48",
+                arrival_date: "2022-06-23T14:48",
+                departure_location: "florence",
+                arrival_location: "rietine"
+            },
+            {
+                title: "rental car to rome",
+                departure_date: "2022-06-28T08:48",
+                arrival_date: "2022-06-28T11:48",
+                departure_location: "rietine",
+                arrival_location: "rome"
+            },
+            {
+                title: "train to naples",
+                departure_date: "2022-06-28T13:48",
+                arrival_date: "2022-06-28T17:48",
+                departure_location: "rome",
+                arrival_location: "naplese"
+            },
+            {
+                title: "train to solerno",
+                departure_date: "2022-06-30T08:48",
+                arrival_date: "2022-06-30T11:48",
+                departure_location: "naples",
+                arrival_location: "solerno"
+            },
+            {
+                title: "bus to praiano",
+                departure_date: "2022-06-30T12:48",
+                arrival_date: "2022-06-30T14:48",
+                departure_location: "solerno",
+                arrival_location: "praiano"
+            },
+            {
+                title: "bus to solerno",
+                departure_date: "2022-06-04T07:48",
+                arrival_date: "2022-06-04T09:48",
+                departure_location: "praiano",
+                arrival_location: "solerno"
+            },
+            {
+                title: "train to Rome",
+                departure_date: "2022-06-23T10:48",
+                arrival_date: "2022-06-23T15:48",
+                departure_location: "solerno",
+                arrival_location: "rome"
+            }
+        ],
+        stays: [
+            {
+                title: "Rome airbnb",
+                checkin_date: "2022-06-20T02:00",
+                checkout_date: "2022-06-23T11:00",
+                city: "rome"
+            },
+            {
+                title: "Tuscany airbnb",
+                checkin_date: "2022-06-23T02:00",
+                checkout_date: "2022-06-28T11:00",
+                city: "reitine"
+            },
+            {
+                title: "Naples airbnb",
+                checkin_date: "2022-06-28T02:00",
+                checkout_date: "2022-06-30T11:00",
+                city: "naples"
+            },
+            {
+                title: "Praiano airbnb",
+                checkin_date: "2022-06-30T02:00",
+                checkout_date: "2022-07-04T11:00",
+                city: "praiano"
+            },
+            {
+                title: "second Rome airbnb",
+                checkin_date: "2022-07-04T02:00",
+                checkout_date: "2022-07-05T11:00",
+                city: "rome"
+            }
+        ],
+        activities: []
+    };
+
+
+    //for testing
+    // entries = [{}];
+
+
+
+
     let curr_type = entry_types[0];
+    let trip_days = (new Date(trip_config.return_date.value).getTime() - new Date(trip_config.start_date.value).getTime())/(1000*60*60*24);
+    let calender_title = `Your ${trip_days} days trip`;
     export let input_data;
     input_data = input_data.input_data;
     input_data = input_data;
@@ -23,35 +158,106 @@
     const add_entry = () => {
         let vals = document.getElementsByTagName('input');
         let result = {};
+        let vals_exist = true;
+        let is_config = false;
         [...vals].forEach(element => {
+            if (!element.value) vals_exist = false;
+            if (element.name == 'start_location' && element.value != '' ) {
+                trip_config.default.value = false;
+                is_config = true;
+            }
+            if (element.name == "start_date") trip_config.start_date.value = element.value;
+            if (element.name == "return_date") trip_config.return_date.value = element.value;
+            if (element.name == "start_location") trip_config.start_location.value = element.value;
             result[element.name] = element.value;
-            element.value = "";
+            
         });
-        entries.push(result);
-        entries = entries;
+
+        if (vals_exist) {
+            [...vals].forEach(element => {
+            element.value = "";
+            });
+            if (!is_config){
+                entries.push(result);
+                entries = entries;
+            }
+            
+           
+        }else {
+            alert("missing value");
+        }
         
+    }
+
+    const get_entries_for_the_day = (day_index, entries) => {
+        let result = [];
+        let day_to_return = daysIntoYear(trip_config.start_date.value) + day_index + 1;
+        entries?.forEach(element => {
+            let start = daysIntoYear(element.departure_date ? element.departure_date : element.checkin_date);
+            let end = daysIntoYear(element.arrival_date ? element.arrival_date : element.checkout_date);
+            if (    (start == day_to_return && end == day_to_return) ||
+                    (start <= day_to_return && end >= day_to_return)){
+                result.push(element);
+            }
+        });
+        return result;
+    }
+
+    const daysIntoYear = (date) => {
+        date = new Date(date);
+        return (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) - Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
+    }
+    const buildCalender = () => {
+        let start = new Date(trip_config.start_date.value);
+        let end = new Date(trip_config.return_date.value);
+        let difference_in_time = end.getTime() - start.getTime();
+        let num_days = (difference_in_time)/(1000*60*60*24);
+        let visual_area = document.getElementById('visual');
+        let calender = document.createElement("div");
+
+
+        let calender_title = document.createElement("div");
+        calender_title.style.width = "100%";
+        calender_title.style.backgroundColor = "pink";
+        calender_title.innerHTML = `Your ${num_days.toString()} day trip`;
+
+        let calender_main = document.createElement("div");
+        
+
+        calender.appendChild(calender_title);
+        visual_area?.append(calender);
     }
     </script>
     
     <div id="main_container">
-        {#each entries as curr}
-        <div class="entry_container">
-            {#each Object.entries(curr) as [key, val]}
-                <div class="row">
-                    <div>
-                        {key}: 
+        <div class="row" id="visual">
+            {#each Array(trip_days) as _, i}
+                <div class="day">
+                    <div class="date" id="date-{i}">day {i+1}</div>
+                    <div class="stay" id="stay-{i}">
+                        {#each get_entries_for_the_day(i, entries.stays) as {title}}
+                            <div class="card_text">{title}</div>
+                        {/each}
                     </div>
-                    <div>
-                        {val}
-                    </div>
+                    <div class="activity" id="activity-{i}">activity</div>
+                    <div class="travel" id="travel-{i}">travel</div>
                 </div>
             {/each}
         </div>
             
-        {/each}
     
         <div id="new_entry_container">
             
+            {#if trip_config.default.value}
+                <div class="row">
+                    {#each Object.entries(trip_config) as [key, val]}
+                        {#if key != 'default'}
+                            <span for={key}>{key}</span>
+                            <div class="input"><input type="{val.type}" value={val.value} name={key}></div>
+                        {/if}
+                    {/each}
+                </div>
+            {:else}
             <div class="row dropdown"><span >Type</span><div class="input"><select bind:value={curr_type} type="dropdown" name="Type">
                 {#each entry_types as curr}
                     <option value={curr}>{curr}</option>
@@ -60,10 +266,11 @@
                 <div class="row">
                     {#each rows as curr}
                         <span for={curr.name}>{curr.name}</span>
-                        <div class="input"><input type="text" name={curr.name}></div>
+                        <div class="input"><input type="{curr.type}" name={curr.name}></div>
                     {/each}
                 </div>
                 {/each}
+            {/if}
     
             
             <button class="button" on:click={add_entry}>ADD</button>
@@ -114,6 +321,7 @@
     }
     select {
         margin:5px;
+        appearance: none;
     }
     
     .button {
@@ -147,6 +355,10 @@
     .button:hover {
       box-shadow: rgba(44,187,99,.35) 0 -25px 18px -14px inset,rgba(44,187,99,.25) 0 1px 2px,rgba(44,187,99,.25) 0 2px 4px,rgba(44,187,99,.25) 0 4px 8px,rgba(44,187,99,.25) 0 8px 16px,rgba(44,187,99,.25) 0 16px 32px;
       transform: scale(1.05) rotate(-1deg);
+    }
+
+    .card_text {
+        font-size: 12px;
     }
     
     @media only screen and (min-width: 600px) {
