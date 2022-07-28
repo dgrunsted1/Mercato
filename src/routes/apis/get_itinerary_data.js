fe48deef05dcfbddbfb284cb3f0d66b528b1d72d
dgrunsted1 <dgrunsted1@gmail.com>
2022-07-28 17:54:57 -0500

    quick fix

--- src/routes/apis/get_itinerary_data.js
+++ src/routes/apis/get_itinerary_data.js
@@ -13,7 +13,6 @@ export async function get(){
     let stay_b = [];
     let type = "activity";
     let query = `SELECT * FROM itinerary_inputs`;
-    console.log({query});
     let results = await mysqlconn.query(query)
         .then(function([rows, result, fields]) {
             return rows;
@@ -21,17 +20,16 @@ export async function get(){
 
     for (let i = 0; i < results.length; i++){
         if (first_row.includes(results[i].name)){
-            if (results[i].activity) activity_a.push({"name": results[i].name, "type": results[i].type});
-            if (results[i].travel) travel_a.push({"name": results[i].name, "type": results[i].type});
-            if  (results[i].stay) stay_a.push({"name": results[i].name, "type": results[i].type});
+            if (results[i].activity == '1') activity_a.push({"name": results[i].name, "type": results[i].type});
+            if (results[i].travel == '1') travel_a.push({"name": results[i].name, "type": results[i].type});
+            if  (results[i].stay == '1') stay_a.push({"name": results[i].name, "type": results[i].type});
         } else {
-            if (results[i].activity) activity_b.push({"name": results[i].name, "type": results[i].type});
-            if (results[i].travel) travel_b.push({"name": results[i].name, "type": results[i].type});
-            if  (results[i].stay) stay_b.push({"name": results[i].name, "type": results[i].type});
+            if (results[i].activity == '1') activity_b.push({"name": results[i].name, "type": results[i].type});
+            if (results[i].travel == '1') travel_b.push({"name": results[i].name, "type": results[i].type});
+            if  (results[i].stay == '1') stay_b.push({"name": results[i].name, "type": results[i].type});
         }
     }
     let input_data ={"activity" : [activity_a, activity_b], "travel" : [travel_a, travel_b], "stay" : [stay_a, stay_b]};
-    console.log({input_data});
     return {
         body: { input_data }
     };
